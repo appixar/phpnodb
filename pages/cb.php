45c9732428da8ef14c92033a392964fba630ec64
J. Williams Balaniuc <jwbalaniuc@gmail.com>
2021-10-02 16:49:44 -0300

    update

--- pages/cb.php
+++
@@ -1,1 +0,0 @@
-<a href='./' class='btn btn-primary btn-md'><i class="fa fa-arrow-left"></i> &nbsp; Voltar</a>
--- pages/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-//
-$dev = "";
-if ($_SESSION['logged']['dev']) $dev = "?dev=1";
-//
-session_destroy();
-header("Location: ../login.php$dev");
-exit;
